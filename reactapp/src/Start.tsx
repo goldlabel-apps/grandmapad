f99115878db8c5581a29298d3979e8b5d93aae65
listingslab <listingslab@gmail.com>
2024-12-13 10:11:22 +0000

    material UI themed and enabled

--- reactapp/src/Start.tsx
+++ reactapp/src/Start.tsx
@@ -9,48 +9,55 @@ import {
   CardContent,
   Typography,
 } from "@mui/material";
+import {Theme} from "./theme";
+
 
 export interface StartProps {
   id: string;
 }
 
 const Start: React.FC<StartProps> = ({ id }) => {
-  return (
-    <div id={id}>
-      <Card sx={{ width: 320 }}>
-        <CardMedia
-          image={config.og}
-          alt={config.appTitle}
-          component="img"
-          height="170"
-        />
-        <CardHeader title={`${config.description}`} />
-        <CardContent>
-          <Grid container spacing={2}>
-            <Grid
-              size={{
-                xs: 12,
-                sm: 6,
-              }}
-            >
-              <Typography variant="body1" sx={{ mb: 2 }}>
-                L
-              </Typography>
+
+  const darkmode = true;
+  const customTheme = config.theme[darkmode ? "dark" : "light"];
+
+  return (<Theme theme={customTheme}>
+      <div id={id}>
+        <Card sx={{ width: 320 }}>
+          <CardMedia
+            image={config.og}
+            alt={config.appTitle}
+            component="img"
+            height="170"
+          />
+          <CardHeader title={`${config.description}`} />
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid
+                size={{
+                  xs: 12,
+                  sm: 6,
+                }}
+              >
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  L
+                </Typography>
+              </Grid>
+              <Grid
+                size={{
+                  xs: 12,
+                  sm: 6,
+                }}
+              >
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                  R
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid
-              size={{
-                xs: 12,
-                sm: 6,
-              }}
-            >
-              <Typography variant="body1" sx={{ mb: 2 }}>
-                R
-              </Typography>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Theme>
   );
 };
 
